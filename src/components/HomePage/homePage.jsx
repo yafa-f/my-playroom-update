4a8ae7927920abf21a96b04763a8a602c43580d5
gila <gw38452@gmail.com>
2024-10-08 12:01:51 +0300

    finish fines page

--- src/components/HomePage/homePage.jsx
+++ src/components/HomePage/homePage.jsx
@@ -3,17 +3,25 @@ import "./homePage.css";
 import { Login } from "../Login/login";
 import { SideBar } from "../SideBar/sideBar";
 import { useLocation } from "react-router-dom";
-
+import { useState } from "react";
 export const HomePage = () => {
   const location = useLocation();
+
+  const [name, setName] = useState("");
   const myLocation = location.pathname;
   return (
     <div className="image">
-      <div className="nav-bar"></div>
+      <div className="nav-bar">
+        <div className="display-login-name">
+          {name===""||name==="שם משתמש"? <div></div> :  <div className="user-icon"></div>}
+      
+          <div className="login-user-name">{name}</div>
+        </div>
+      </div>
       <div className="logo"></div>
       {myLocation == "/" ? (
         <div className="login-comp">
-          <Login />
+          <Login setName={setName} />
         </div>
       ) : (
         <div className="side-bar-comp">
@@ -23,4 +31,3 @@ export const HomePage = () => {
     </div>
   );
 };
-
